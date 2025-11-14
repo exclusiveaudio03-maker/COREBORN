099407e625761e38fe1e8789a4bf2f2b4b261dbe
exclusiveaudio03-maker <exclusiveaudio03@gmail.com>
2025-11-14 20:14:35 +0000

    Create player_1st.gd

---
+++ scripts/player/player_1st.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+
+var speed := 5.0
+var camera
+
+func _ready():
+	camera = $Camera3D_First
+
+func _physics_process(delta):
+	var input = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_forward"):
+		input.z -= 1
+	if Input.is_action_pressed("move_back"):
+		input.z += 1
+	if Input.is_action_pressed("move_left"):
+		input.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input.x += 1
+
+	input = input.normalized()
+	velocity = (global_transform.basis * input) * speed
+	move_and_slide()
