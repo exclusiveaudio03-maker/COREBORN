108b1a5be0c100f3acd135b6e5b2b94f9082755b
exclusiveaudio03-maker <exclusiveaudio03@gmail.com>
2025-11-14 17:37:37 +0000

    Update ChoiceManager.gd

--- scripts/autoload/ChoiceManager.gd
+++ scripts/autoload/ChoiceManager.gd
@@ -1,1 +1,18 @@
+extends Node
+signal choice_applied(id, value)
 
+func apply_choice(id:String, value) -> void:
+	GameState.set_choice(id, value)
+	match id:
+		"act1_cooperate_gov":
+			if value == "gov":
+				GameState.save_data["reputation"]["government"] += 10
+			else:
+				GameState.save_data["reputation"]["corebreakers"] += 10
+		"act3_cure_or_destroy":
+			if value == "cure":
+				GameState.save_data["reputation"]["scientists"] += 15
+			else:
+				GameState.save_data["reputation"]["military"] += 15
+	GameState.save()
+	emit_signal("choice_applied", id, value)
